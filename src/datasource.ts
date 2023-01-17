28b65e9ea97a24d527d7c9edbd1af53a0da0f802
jovanamatk <mat.jovana@gmail.com>
2023-01-17 22:49:04 +0100

    feat: integrate a custom DataSource to support TypeORM CLI

---
+++ src/datasource.ts
@@ -0,0 +1,18 @@
+// dataSource.ts
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const dataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: +process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: ['dist/*.entity.js'],
+  migrations: ['dist/migrations/*.js'],
+});
+
+export default dataSource;
